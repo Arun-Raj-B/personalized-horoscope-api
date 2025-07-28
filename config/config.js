41e4c00748eb6bc4cd9ae38969ebf0c95a22c523
arunrajb <sent2arunraj@gmail.com>
2025-07-28 20:30:44 +0530

    Add User and HoroscopeHistory models and migrations
    Introduces Sequelize models and migrations for User and HoroscopeHistory, establishing their associations. Refactors config and models/index.js to use CommonJS, and removes 'type': 'module' from package.json for compatibility.

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config();
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -7,7 +6,6 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'postgres',
   }
-};
+}
