071383ca0a63358e0bd8b36cf0e8d8d81de8ee52
arunrajb <sent2arunraj@gmail.com>
2025-07-29 01:31:46 +0530

    Add rate limiting to horoscope routes
    Introduced a new rate limiter middleware using express-rate-limit to restrict users to 5 requests per minute on horoscope routes. Updated dependencies to include express-rate-limit and applied the middleware to all horoscope endpoints to prevent abuse.

--- routes/horoscope.routes.js
+++ routes/horoscope.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
 const { horoscopeToday, horoscopeHistory } = require('../controllers/horoscope.controllers');
+const horoscopeRateLimiter = require('../middlewares/rate-limitter.middleware');
 
 router.use(authMiddleware);
+router.use(horoscopeRateLimiter);
 
 router.get('/today', horoscopeToday);
 router.get('/history', horoscopeHistory);
